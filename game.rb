7872f3b41e439745b813a71db42ea44c74e58753
Sam Eff <HeySamEff@gmail.com>
2020-06-16 15:05:09 -0700

    computer will now choose a random word between 5 and 12 characters long

--- game.rb
+++ game.rb
@@ -3,6 +3,50 @@ require 'pry'
 #plays game
 class Game
 
+  def initialize
+    @word_master = WordMaster.new
+    @word_guesser = WordGuesser.new
+    @good_guesses = Array.new(@code_word.length, '_')
+    @bad_guesses = []
+    @tries = 0
+  end
+
+  #plays game
+  def play_game
+
+  end
+
+  #saves game to finish later
+  def save_game
+
+  end
+
+  #loads unfinished game
+  def load_game
+
+  end
+
+  #saves guess to appropriate array
+  def save_guess
+    if good_guess?
+      matching_indexes(@word_master.code_word, @word_guesser.guess).each do |i|
+        @good_guesses[i] = @word_guesser.guess
+      end
+    else
+      @bad_guesses.push(@word_guesser.guess)
+    end
+  end  
+  
+  #checks if guess is good
+  def good_guess?
+    @word_master.code_word.include?(@word_guesser.guess)
+  end
+
+  #returns array of all matching indexes for 
+  def matching_indexes(str, target)
+    (0..str.length-1).select { |i| str[i] == target}
+  end
+
 end
 
 #gets a code word at random from word_list.txt 
@@ -15,19 +59,22 @@ class WordMaster
 
   #chooses code word from word_list
   def choose_code_word
-
+    dictionary = []
+    File.readlines("word_list.txt").each do |word|
+      if word.length > 4 && word.length < 13
+        dictionary.push(word.strip.downcase)
+      end
+    end
+    @code_word = dictionary[rand(dictionary.length)]
   end
 end
 
-#gets guesses from user and compares guesses to master word
+#gets guesses from user
 class WordGuesser
-  attr_reader :guess, :good_guesses, :bad_guesses
+  attr_reader :guess
 
-  def initialize(code_word)
-    @code_word = code_word
+  def initialize
     @guess = ''
-    @good_guesses = Array.new(@code_word.length, '_')
-    @bad_guesses = []
   end
 
   #gets new letter guess from user
@@ -43,32 +90,8 @@ class WordGuesser
   def valid_letter?(input)
     input.length == 1 && input.match?(/^[[:alpha:]]+$/)
   end
-
-  #saves guess to appropriate array
-  def save_guess
-    if good_guess?
-      matching_indexes(@code_word, @guess).each do |i|
-        @good_guesses[i] = @guess
-      end
-    else
-      @bad_guesses.push(@guess)
-    end
-  end  
-  
-  #checks if guess is good
-  def good_guess?
-    @code_word.include?(@guess)
-  end
-
-  #returns array of all matching indexes for 
-  def matching_indexes(str, target)
-    (0..str.length-1).select { |i| str[i] == target}
-  end
-
 end
 
-pl = WordGuesser.new('hello')
-pl.get_new_letter
-pl.save_guess
-p pl.good_guesses
-p pl.bad_guesses
+m = WordMaster.new
+m.choose_code_word
+p m.code_word
