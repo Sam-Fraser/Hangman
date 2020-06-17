cca9d0b04d842c75bc925f3d36c9c1a0ebf88567
Sam Eff <HeySamEff@gmail.com>
2020-06-16 18:00:31 -0700

    created save_files dir for save files and created saving a loading features for game

--- game.rb
+++ game.rb
@@ -1,7 +1,9 @@
 require 'pry'
+require 'json'
 
 #plays game
 class Game
+  attr_reader :tries, :bad_guesses, :good_guesses, :code_word
 
   def initialize
     @word_master = WordMaster.new
@@ -30,57 +32,83 @@ class Game
     #get new code word
     @word_master.choose_code_word
     @code_word = @word_master.code_word
-    @good_guesses = Array.new(@code_word.length, '_')
+    @good_guesses = Array.new(@code_word.length, '_ ')
     #tells you rules
-    puts "Try to figure out the word. If you guess 5 wrong letters, you lose!"
+    puts "Try to figure out the word. If you guess 6 wrong letters, you lose!"
     #starts playing
     game_loop
   end
 
   #continues loaded game
   def continue_game(code_word, good_guesses, bad_guesses, tries)
-
+   #sets all values from saved game
+    @code_word = code_word
+    @good_guesses = good_guesses
+    @bad_guesses = bad_guesses
+    @tries = tries
+   #tells you rules
+   puts "Try to figure out the word. If you guess 6 wrong letters, you lose!"
+   #starts playing
+   game_loop
   end
 
   #saves game to finish later
   def save_game
+    Dir.mkdir 'save_files' unless Dir.exists?("save_files")
 
+    filename = "save#{Dir["save_files/**/*"].length}.json"
+
+    save_data = {
+      code_word: @code_word,
+      good_guesses: @good_guesses,
+      bad_guesses: @bad_guesses,
+      tries: @tries
+    }
+
+    File.write("save_files/#{filename}", JSON.dump(save_data))
   end
 
   #loads unfinished game
   def load_game
     #lists all save files
-
+    Dir.entries("save_files").each { |file| puts file[0...file.index('.')]}
     #asks which save file user would like to load
-    puts "Which save file would you like to load? (type name here)"
+    puts "Which save file would you like to load? (type whole name here)"
     save_file = gets.chomp.downcase
-
     #loads save file
-
+    save_data = JSON.load(File.read("save_files/#{save_file}.json"))
     #continues game
-    continue_game()
+    continue_game(save_data['code_word'], save_data['good_guesses'], save_data['bad_guesses'], save_data['tries'])
   end
 
   #basic game loop
   def game_loop
     loop do 
-      #tells you how many tries you have left
-      puts "You have #{5-@tries} bad guesses left."
+      #tells you how many tries you have left and what you've already guessed
+      puts "You have #{6-@tries} bad guesses left."
+      puts "Bad Guesses:  #{@bad_guesses.join(', ')}"
+      puts "Current Word: #{@good_guesses.join('')}"
       #get guess from player
       @word_guesser.get_new_letter
       #saves guess to appropriate array
       save_guess
       #increases tries unless the guess was good
       @tries += 1 unless good_guess?
+      puts "good guess" if good_guess?
+      puts "bad guess" unless good_guess?
       if win?
         puts "Congrats! You Win! The word was #{@code_word}"
         break
-      elsif @tries == 5
+      elsif @tries == 6
         puts "Shucks! You lost. The word was #{@code_word}"
         break
       end
-      puts "Bad Guesses:  #{@bad_guesses.join(', ')}"
-      puts "Current Word: #{@good_guesses.join('')}"
+      puts "save game? (y,n)"
+      save_state = gets.chomp.downcase
+      if save_state == 'y'
+        save_game
+        break
+      end
     end
   end
 
@@ -156,4 +184,4 @@ class WordGuesser
 end
 
 g = Game.new
-g.play_new_game
+g.play_game
